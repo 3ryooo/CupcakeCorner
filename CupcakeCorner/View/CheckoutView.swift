b215592c96a23efaea58ce3e67130fab16f9b525
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-06 17:46:50 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/sending-and-receiving-orders-over-the-internet

--- CupcakeCorner/View/CheckoutView.swift
+++ CupcakeCorner/View/CheckoutView.swift
@@ -8,6 +8,9 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -23,13 +26,51 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: { })
-                    .padding()
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check out")
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("reqres-free-v1", forHTTPHeaderField: "x-api-key")
+        request.httpMethod = "POST"
+        
+        do {
+            print("test")
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            print("test2")
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            print("test3")
+            showingConfirmation = true
+        } catch {
+            print(error)
+            print("Checkout failed: \(error.localizedDescription)")
+        }
+        
+        
+        
     }
 }
 
