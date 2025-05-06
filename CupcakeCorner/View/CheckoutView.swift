ac7e0f7214abca058ecf01ae0029d71e9a263b1a
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-06 13:35:46 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/checking-for-a-valid-address

---
+++ CupcakeCorner/View/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
